164d22298073f96ecba08d3f1088efe90a12db10
shy <shyamal.punekar@gmail.com>
2017-09-21 10:32:39 -0700

    remove commented lines

--- app/app.component.ts
+++ app/app.component.ts
@@ -51,39 +51,3 @@ export class Recipe{
   constructor(public title: String, public ingredients: String[] , public directions: String[]){}
 
 }
-
-// import { Component } from '@angular/core';
-//
-// @Component({
-//   selector: 'app-root',
-//   template: `
-//   <div class= "container">
-//   <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-//   <h3>{{currentFocus}}</h3>
-//   <ul>
-//     <li *ngFor = "let currentTask of tasks">{{currentTask.description}}</li>
-//   </ul>
-//   </div>
-//   `
-// })
-//
-// export class AppComponent {
-//   currentFocus: String = 'Angular Homework';
-//   currentTime = new Date();
-//   month: number = this.currentTime.getMonth() + 1;
-//   day: number = this.currentTime.getDate();
-//   year: number = this.currentTime.getFullYear();
-//   //firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
-//   tasks: Task[] = [
-//     new Task('Finish weekend Angular homework for Epicodus course'),
-//     new Task('Begin brainstorming possible JavaScript group projects'),
-//     new Task('Add README file to last few Angular repos on GitHub')
-//   ];
-// }
-//
-//   export class Task{
-//     public done: boolean = false;
-//
-//     constructor(public description: String){}
-//
-//   }
